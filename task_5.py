e21bd12ea3de466ee7f35407d57c9324b62a0ffb
Koyani <jabox@yandex.ru>
2019-01-07 20:03:41 +0300

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,2 +1,16 @@
 #task_5
 #В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию в массиве.
+from random import random
+m = []
+for n in range(0, 10):
+    m.append(randint(-100, 100))
+print(m)
+if m[i] < 0:
+    maxx = m[i]
+for i in range(len(m)):
+    if (m[i] < 0) and (m[i] > maxx):
+        maxx = m[i]
+        print(maxx)
+for i in range(len(m)):        
+    if m[i] == maxx:
+print(f"Maximum value of all negative elements in array is {maxx} and its index is {i}") 
