573f3058078bb4f31ec239e4a8e279d43d2ad6d0
Koyani <jabox@yandex.ru>
2019-01-07 18:33:26 +0300

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,4 +1,22 @@
 #task_7
 #В одномерном массиве целых чисел определить два наименьших элемента. 
 #Они могут быть как равны между собой (оба являться минимальными), так и различаться.
+m = [-9, 8, -8, 0, 5, 4, -4, -4]
+minn = m[0]
+for i in range(len(m)):
+    if m[i] < minn:
+        minn = m[i]
+for i in range(len(m)):        
+    if m[i] == minn:
+        min_i = i
+m.remove(minn)
+print(f"First minimal number: {minn}")
+minn = m[0]
+for i in range(len(m)):
+    if m[i] < minn:
+        minn = m[i]
+for i in range(len(m)):        
+    if m[i] == minn:
+        min_i = i  
+print(f"Second minimal number: {minn}")
 
