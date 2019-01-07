f3423c679a1971bcc701c7657e3d984d38934559
Koyani <jabox@yandex.ru>
2019-01-07 17:05:23 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,2 +1,22 @@
 #task_3
 #В массиве случайных целых чисел поменять местами минимальный и максимальный элементы.
+from random import random
+m = []
+for n in range(0, 10):
+    m.append(randint(-100, 100))
+print(m)
+max = m[i]
+min = m[i]
+for i in range(len(m)):
+    if m[i] > max:
+        max = m[i]
+    if m[i] < min:
+        min = m[i]
+for i in range(len(m)):
+    if m[i] == max:
+        max_i = i
+    if m[i] == min:
+        min_i = i
+m[max_i] = min
+m[min_i] = max
+print(m)
