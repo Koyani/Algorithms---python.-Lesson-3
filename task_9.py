e6435aaccb606cd7befc3b8faaa43375e141ebac
Koyani <jabox@yandex.ru>
2019-01-07 20:42:17 +0300

    Update task_9.py

--- task_9.py
+++ task_9.py
@@ -1,2 +1,24 @@
 #task_9
 #Найти максимальный элемент среди минимальных элементов столбцов матрицы.
+from random import random
+x = 5
+y = 4
+m = []
+for i in range(x):
+    n = []
+    for j in range(x):
+        number = int(randint(0, 100))
+        n.append(number)
+        print(number, ' ',end='')
+    m.append(n)
+    print('')
+ 
+maxx = -1
+for j in range(x):
+    minn = 10000
+    for i in range(y):
+        if m[i][j] < minn:
+            minn = m[i][j]
+    if minn > maxx:
+        maxx = minn
+print("Maximum among minimal: ", maxx)
